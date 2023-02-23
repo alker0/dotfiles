1e60f34ac4b086ae15d13a15293b1e8c297bdff9
alker0 <alker8h@gmail.com>
2023-02-24 07:58:52 +0900

    feat(bash)!: Improve bash configuration

--- .chezmoitemplates/shell/bash-alias.sh
+++ .chezmoitemplates/shell/bash-alias.sh
@@ -7,40 +7,78 @@
 {{-   if empty $depends_list -}}
 alias {{ get . "name" }}='{{ get . "command" }}'
 {{    else }}
+{{-     $alias_indent := "" }}
+{{-     $missing_depends := true }}
+{{-     $check_on_run := get . "check_on_run" }}
+{{-     if not $check_on_run }}
+{{-       range $depends_info := $depends_list }}
+{{-         if $missing_depends }}
+{{-           $depends := get $depends_info "depends" }}
+{{-           if lookPath $depends }}
+{{-             $depends_list = list $depends_info }}
+{{-             $missing_depends = false }}
+{{-           end }}
+{{-         end }}
+{{-       end }}
+{{-       if $missing_depends }}
+{{-         $depends_list = list }}
+{{-       end }}
+{{-     end }}
 {{-     "\n" }}
 {{-     range $index, $depends_info := $depends_list }}
 {{-       $depends := get $depends_info "depends" }}
-{{-       if $depends }}
-{{-         if $index | eq 0 -}}
+{{-       if $check_on_run }}
+{{-         if $depends }}
+{{-           if $index | eq 0 -}}
 if
-{{-         else -}}
+{{-             $alias_indent = "  " }}
+{{-             $missing_depends = false }}
+{{-           else }}
 elif
-{{-         end }}{{- " " -}}
-  [ -n "$(command -v {{ $depends }})" ]; then
-{{-       else -}}
-else
-{{-       end -}}
-{{-       "\n" }}
-{{-       range get . "aliases" }}{{- "  " -}}
-alias {{ get . "name" }}='
-{{-         $is_shutdown := get . "note" | eq "shutdown" }}
-{{-         if $is_shutdown -}}
-{{-           "history -a && exec " }}
+{{-           end }}{{- " " -}}
+  [ -n "$(command -v {{ $depends }})" ]; then{{ "\n" }}
+{{-         else if gt $index 0 }}
+else{{ "\n" }}
 {{-         end }}
-{{-         range $index, $command := get . "commands"
-          | default (get . "command"| list)
-          | compact
-}}
-{{-           if gt $index 0 }}
-{{-             " && " }}
-{{-           end }}
-{{-           printf $command $depends }}
-{{-         end -}}
-'
-{{-         "\n" }}
 {{-       end }}
-{{-     end -}}
-fi
-{{-     "\n" }}
+{{-       if not $missing_depends -}}
+{{-         range get . "aliases" }}
+{{-           $alias := . }}{{- $alias_indent -}}
+alias {{ get $alias "name" }}='
+{{-           $is_shutdown := get $alias "note" | eq "shutdown" }}
+{{-           if $is_shutdown -}}
+{{-             "history -a && exec " }}
+{{-           end }}
+{{-           range $index, $command := get $alias "commands"
+            | default (get $alias "command"| list)
+            | compact
+}}
+{{-             if gt $index 0 }}
+{{-               " && " }}
+{{-             end }}
+{{-             if hasKey $alias "env" }}
+{{-               $env_dic := get $alias "env" }}
+{{-               range $env_name := keys $env_dic }}
+{{-                 printf "%s=" $env_name }}
+{{-                 $env_value := get $env_dic $env_name }}
+{{-                 if $env_value | contains "'" }}
+{{-                   $env_value = $env_value | replace "'" "'\"'\"'" }}
+{{-                 end }}
+{{-                 if $env_value | contains " " }}
+{{-                   printf "%s " ($env_value | squote | quote | squote) }}
+{{-                 else }}
+{{-                   printf "%s " $env_value }}
+{{-                 end }}
+{{-               end }}
+{{-             end }}
+{{-             printf $command $depends }}
+{{-           end -}}
+'{{ "\n" }}
+{{-         end }}
+{{-       end }}
+{{-     end }}
+{{-     if $check_on_run -}}
+fi{{ "\n" }}
+{{-     end }}
 {{-   end }}
 {{- end -}}
